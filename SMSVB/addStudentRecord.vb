200ea5341930534886a8ac97cd5e4bb5ff5fb695
MahmudSuleman <mahmudsheikh25@gmail.com>
2019-08-07 15:07:34 +0000

    sql insert statement changed

--- SMSVB/addStudentRecord.vb
+++ SMSVB/addStudentRecord.vb
@@ -11,7 +11,7 @@ Public Class addStudentRecord
             MYCONN.Open()
             Dim Query As String
             Query =
-"insert into SMS.studentinfor (Id,FirstName,LastName,DOB,Address,Gender,City,District,Class,ParentFullName,Occupation,ParentNumber) values ('" & TextBox8.Text & "','" & TextBox1.Text & "','" & TextBox2.Text & "','" & TextBox3.Text & "','" & ComboBox1.Text & "','" & DateTimePicker1.Text & "','" & TextBox4.Text & "','" & ComboBox2.Text & "','" & TextBox5.Text & "','" & TextBox6.Text & "','" & TextBox7.Text & "')  "
+"insert into SMS.studentinfo (id,FirstName,LastName,DOB,Address,Gender,City,District,Class,ParentFullName,Occupation,ParentNumber) values ('" & TextBox8.Text & "','" & TextBox1.Text & "','" & TextBox2.Text & "','" & TextBox3.Text & "','" & ComboBox1.Text & "','" & DateTimePicker1.Text & "','" & TextBox4.Text & "','" & ComboBox2.Text & "','" & TextBox5.Text & "','" & TextBox6.Text & "','" & TextBox7.Text & "')  "
             command = New MySqlCommand(Query, MYCONN)
             reader = command.ExecuteReader
             MessageBox.Show("Form Submited Successfully. Thank you!!!")
@@ -26,15 +26,15 @@ Public Class addStudentRecord
     End Sub
 
   
-    Private Sub addStudentRecord_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Dim rn As Integer
-        Dim ipper As Integer = 2009
-   '     Dim faculty As String = InputBox("Enter the School Short Code:")
-    '    Dim year As Integer = InputBox("Enter the last two digit of the Admitted year:")
-        Randomize()
+    ' Private Sub addStudentRecord_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    '     Dim rn As Integer
+    '     Dim ipper As Integer = 2009
+    ''     Dim faculty As String = InputBox("Enter the School Short Code:")
+    ' '    Dim year As Integer = InputBox("Enter the last two digit of the Admitted year:")
+    '     Randomize()
 
-        rn = Rnd() * ipper
-   '     TextBox1.Text = faculty & "/" & rn & "/" & year
-        textbox1.text=rn
-    End Sub
+    '     rn = Rnd() * ipper
+    ''     TextBox1.Text = faculty & "/" & rn & "/" & year
+    '     textbox1.text=rn
+    ' End Sub
 End Class
