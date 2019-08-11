d09f2bdada86237b84068bcd1425d083c69ed213
MahmudSuleman <mahmudsheikh25@gmail.com>
2019-08-11 05:54:35 +0000

    login form remodified

--- SMSVB/login.Designer.vb
+++ SMSVB/login.Designer.vb
@@ -29,9 +29,10 @@ Partial Class login
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.Button1 = New System.Windows.Forms.Button()
         Me.Label3 = New System.Windows.Forms.Label()
         Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -61,11 +62,11 @@ Partial Class login
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(190, 144)
+        Me.Label1.Location = New System.Drawing.Point(258, 144)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(315, 37)
+        Me.Label1.Size = New System.Drawing.Size(182, 37)
         Me.Label1.TabIndex = 10
-        Me.Label1.Text = "Administrator Login"
+        Me.Label1.Text = "User Login"
         '
         'PictureBox1
         '
@@ -96,15 +97,6 @@ Partial Class login
         Me.TextBox1.Size = New System.Drawing.Size(235, 20)
         Me.TextBox1.TabIndex = 13
         '
-        'Button1
-        '
-        Me.Button1.Location = New System.Drawing.Point(197, 488)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(235, 31)
-        Me.Button1.TabIndex = 14
-        Me.Button1.Text = "Login"
-        Me.Button1.UseVisualStyleBackColor = True
-        '
         'Label3
         '
         Me.Label3.AutoSize = True
@@ -122,14 +114,33 @@ Partial Class login
         Me.TextBox2.Size = New System.Drawing.Size(235, 20)
         Me.TextBox2.TabIndex = 16
         '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(231, 506)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 17
+        Me.Button1.Text = "ADMIN"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(381, 506)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 18
+        Me.Button2.Text = "TEACHER"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
         'login
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(730, 541)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.TextBox2)
         Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.PictureBox1)
@@ -151,7 +162,8 @@ Partial Class login
     Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
     Friend WithEvents Label2 As System.Windows.Forms.Label
     Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
-    Friend WithEvents Button1 As System.Windows.Forms.Button
     Friend WithEvents Label3 As System.Windows.Forms.Label
     Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
 End Class
--- SMSVB/login.vb
+++ SMSVB/login.vb
@@ -6,38 +6,14 @@ Public Class login
     Dim reader As MySqlDataReader
 
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        conn = New MySqlConnection
-        conn.ConnectionString = "server=localhost;userid=root;password=;database=dahansheli"
-
-        Try
-            conn.Open()
-            Dim query As String
-            query = "SELECT * FROM dahansheli.admin WHERE username='" & TextBox1.Text & "' AND password='" & TextBox2.Text & "'"
-            command = New MySqlCommand(query, conn)
-            reader = command.ExecuteReader
-
-            Dim count As Integer = 0
-
-            While reader.Read
-                count = count + 1
-            End While
-
-            If count = 1 Then
-                Me.Hide()
-                Dahansheli.Show()
-            Else
-                MsgBox("Incorrect Username and Password combination")
-
-            End If
-
-        Catch ex As Exception
-
-        End Try
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
 
 
     End Sub
 
    
    
+    Private Sub login_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
