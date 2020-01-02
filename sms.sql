951026b9d24ee268561013b98a76f4fc0d3316ab
MahmudSuleman <mudisheikh@outlook.com>
2020-01-02 08:39:06 +0000

    data and fields file added

--- sms.sql
+++ sms.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 4.8.4
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Aug 22, 2019 at 06:54 PM
--- Server version: 5.7.23
--- PHP Version: 7.2.10
+-- Host: 127.0.0.1
+-- Generation Time: Jan 07, 2020 at 05:28 PM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 7.3.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -28,18 +28,16 @@ SET time_zone = "+00:00";
 -- Table structure for table `examresult`
 --
 
-DROP TABLE IF EXISTS `examresult`;
-CREATE TABLE IF NOT EXISTS `examresult` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `examresult` (
+  `id` int(11) NOT NULL,
   `firstName` varchar(255) NOT NULL,
   `lastName` varchar(255) NOT NULL,
   `academicYear` varchar(255) NOT NULL,
   `term` varchar(255) NOT NULL,
   `endTerm` varchar(255) NOT NULL,
   `midTerm` varchar(255) NOT NULL,
-  `totalMark` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+  `totalMark` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `examresult`
@@ -50,7 +48,14 @@ INSERT INTO `examresult` (`id`, `firstName`, `lastName`, `academicYear`, `term`,
 (2, 'mudi', 'mudi', '2012/13', 'ONE ', '75', '25', '7525'),
 (3, '', '', '', '', '50', '25', '5025'),
 (4, '', '', '', '', '2', '12', '122'),
-(5, '', '', '', '', '22', '12', '34');
+(5, '', '', '', '', '22', '12', '34'),
+(6, '', '', '', '', '', '', '0'),
+(7, '', '', '', '', '', '', '0'),
+(8, '', '', '', '', '', '', '0'),
+(9, '', '', '', '', '', '', '0'),
+(10, 'v', 'h', '2012/13', 'ONE ', '70', '23', '93'),
+(11, '', '', '', '', '', '', '0'),
+(12, 'Manhmud', 'Suleman', '2012/13', 'ONE ', 'ooo', 'kk', '0');
 
 -- --------------------------------------------------------
 
@@ -58,8 +63,7 @@ INSERT INTO `examresult` (`id`, `firstName`, `lastName`, `academicYear`, `term`,
 -- Table structure for table `staffinfor`
 --
 
-DROP TABLE IF EXISTS `staffinfor`;
-CREATE TABLE IF NOT EXISTS `staffinfor` (
+CREATE TABLE `staffinfor` (
   `StaffId` varchar(30) NOT NULL,
   `FullName` varchar(100) NOT NULL,
   `Gender` varchar(15) DEFAULT NULL,
@@ -69,8 +73,7 @@ CREATE TABLE IF NOT EXISTS `staffinfor` (
   `SubjectTeaching` varchar(100) NOT NULL,
   `DOB` varchar(16) DEFAULT NULL,
   `DateAdmitted` varchar(15) DEFAULT NULL,
-  `Hobby` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`StaffId`)
+  `Hobby` varchar(100) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
@@ -78,8 +81,8 @@ CREATE TABLE IF NOT EXISTS `staffinfor` (
 --
 
 INSERT INTO `staffinfor` (`StaffId`, `FullName`, `Gender`, `TelNumber`, `Region`, `City`, `SubjectTeaching`, `DOB`, `DateAdmitted`, `Hobby`) VALUES
-('mudi1234', 'Mudi Sheikh', 'female', '289643678', 'NORTHERN', 'Joli', 'French', '8/12/2019', '8/12/2019', 'eating'),
-('Rash2211', 'Zakaria Rashida', 'FEMALE', '05533432', 'NORTHERN', 'Tamale', 'Biochem', '8/12/2019', '8/12/2019', 'Reading');
+('rash2211', 'Mudi Sheikh', 'female', '289643678', 'NORTHERN', 'Tamale,Ghana', 'French', '8/12/2019', '8/12/2019', 'FootBall'),
+('abcd', 'mmmmmm', 'male', '554555', 'GREATER ACCRA', 'jjjhh', 'bhhj', '8/28/2019', '8/28/2019', 'oiuu');
 
 -- --------------------------------------------------------
 
@@ -87,22 +90,24 @@ INSERT INTO `staffinfor` (`StaffId`, `FullName`, `Gender`, `TelNumber`, `Region`
 -- Table structure for table `studentinfor`
 --
 
-DROP TABLE IF EXISTS `studentinfor`;
-CREATE TABLE IF NOT EXISTS `studentinfor` (
+CREATE TABLE `studentinfor` (
   `StudentId` varchar(150) NOT NULL,
   `FirstName` varchar(100) NOT NULL,
   `Lastname` varchar(100) NOT NULL,
+  `otherName` varchar(200) NOT NULL,
   `DOB` varchar(100) NOT NULL,
-  `Address` varchar(50) NOT NULL,
   `Gender` varchar(10) DEFAULT NULL,
-  `City` varchar(30) DEFAULT NULL,
-  `District` varchar(100) DEFAULT NULL,
   `Class` varchar(20) DEFAULT NULL,
-  `ParentContact` varchar(200) DEFAULT NULL,
-  `ParentFullName` varchar(100) DEFAULT NULL,
-  `Occupation` varchar(150) DEFAULT NULL,
-  `MOMONumber` varchar(16) DEFAULT NULL,
-  PRIMARY KEY (`StudentId`)
+  `FatherContact` varchar(200) DEFAULT NULL,
+  `FatherFirstName` varchar(100) DEFAULT NULL,
+  `FatherLastName` varchar(150) DEFAULT NULL,
+  `FatherAddress` varchar(200) DEFAULT NULL,
+  `FatherEmail` varchar(200) NOT NULL,
+  `MotherFirstName` varchar(200) NOT NULL,
+  `MotherLastName` varchar(200) NOT NULL,
+  `MotherContact` varchar(20) NOT NULL,
+  `MotherAddress` varchar(200) NOT NULL,
+  `MotherEmail` varchar(200) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -111,8 +116,7 @@ CREATE TABLE IF NOT EXISTS `studentinfor` (
 -- Table structure for table `users`
 --
 
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
+CREATE TABLE `users` (
   `UserId` varchar(20) DEFAULT NULL,
   `UserType` varchar(100) DEFAULT NULL,
   `Password` varchar(100) DEFAULT NULL
@@ -124,7 +128,40 @@ CREATE TABLE IF NOT EXISTS `users` (
 
 INSERT INTO `users` (`UserId`, `UserType`, `Password`) VALUES
 ('mudi1234', 'admin', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4'),
-('Rash2211', 'teacher', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4');
+('Rash2211', 'teacher', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4'),
+('abcd', 'teacher', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `examresult`
+--
+ALTER TABLE `examresult`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `staffinfor`
+--
+ALTER TABLE `staffinfor`
+  ADD PRIMARY KEY (`StaffId`);
+
+--
+-- Indexes for table `studentinfor`
+--
+ALTER TABLE `studentinfor`
+  ADD PRIMARY KEY (`StudentId`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `examresult`
+--
+ALTER TABLE `examresult`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
